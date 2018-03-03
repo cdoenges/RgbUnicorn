17c4f0f845713bd86a7f36c4f8c8c7a726071b53
Christian Dönges <cd@platypus-projects.de>
2018-03-03 23:40:43 +0100

    Down animation.

--- RgbUnicorn.ino
+++ RgbUnicorn.ino
@@ -7,14 +7,14 @@ constexpr int BRIGHTNESS = 48;
 
 // The color sequence that we'll be animating.
 const CRGB colors[NUM_COLORS] = {
-	CHSV(HUE_RED, 255, BRIGHTNESS),
-	CHSV(HUE_ORANGE, 255, BRIGHTNESS),
-	CHSV(HUE_YELLOW, 255, BRIGHTNESS),
-	CHSV(HUE_GREEN, 255, BRIGHTNESS),
-	CHSV(HUE_AQUA, 255, BRIGHTNESS),
-	CHSV(HUE_BLUE, 255, BRIGHTNESS),
-	CHSV(HUE_PURPLE, 255, BRIGHTNESS),
-	CHSV(HUE_PINK, 255, BRIGHTNESS),
+    CHSV(HUE_RED, 255, BRIGHTNESS),
+    CHSV(HUE_ORANGE, 255, BRIGHTNESS),
+    CHSV(HUE_YELLOW, 255, BRIGHTNESS),
+    CHSV(HUE_GREEN, 255, BRIGHTNESS),
+    CHSV(HUE_AQUA, 255, BRIGHTNESS),
+    CHSV(HUE_BLUE, 255, BRIGHTNESS),
+    CHSV(HUE_PURPLE, 255, BRIGHTNESS),
+    CHSV(HUE_PINK, 255, BRIGHTNESS),
 };
 CRGBArray<NUM_LEDS> leds;
 
@@ -25,7 +25,8 @@ void setup() {
 } // setup()
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    // put your main code here, to run repeatedly:
+    // Move the string of colors upwards.
     for (int dot_offset = 0;dot_offset < NUM_LEDS;dot_offset ++) {
         for (int dot = 0; dot < NUM_LEDS; dot ++) {
             CRGB color;
@@ -41,4 +42,29 @@ void loop() {
         FastLED.show();
         delay(100);
     } // for dot_offset
+
+    // Now the colors fall back down.
+    for (int dot_offset = NUM_LEDS;dot_offset >= 0;dot_offset --) {
+        for (int dot = 0; dot < NUM_LEDS; dot ++) {
+            CRGB color;
+            if (dot < dot_offset) {
+                color = CRGB::Black;
+            } else if (dot >= dot_offset + NUM_COLORS) {
+                color = CRGB::Black;
+            } else {
+                color = colors[dot - dot_offset];
+            }
+            leds[dot] = color;
+        } // for dot
+        FastLED.show();
+        delay(10);
+    } // for dot_offset
+
+    // All off.
+    memset(leds, 0, sizeof(leds));
+//    for (int dot = 0;dot < NUM_LEDS;dot ++) {
+//        leds[dot] = CRGB::Black;
+//    } // for dot
+    FastLED.show();
+    delay(500);
 } // loop()
